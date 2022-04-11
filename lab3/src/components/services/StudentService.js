01dc37a139a6dac2337f06192136d6cfe61cdf61
Kacper <kacper-sleziak@wp.pl>
2022-04-11 20:21:55 +0200

    Update StudentService.js

--- lab3/src/components/services/StudentService.js
+++ lab3/src/components/services/StudentService.js
@@ -1,83 +1,23 @@
-const students = [
-    {
-        id: 1, 
-        firstName: "Jan", 
-        lastName: "Kowalski", 
-        email: "jkowalski@gmail.com",
-        subject:"PIW", 
-        tags:"PIW", 
-        description: "Poszukuje osoby do projektu "
-    },
-    
-    {
-        id: 2, firstName: "Grzegorz", 
-        lastName: "Kowalski", 
-        email: "gkowalski@gmail.com", 
-        subject:"SO2",
-        tags:"Szandała Bash", 
-        description: "Poszukuje osoby do projektu "
-    },
 
-    {
-        id: 3, 
-        firstName: "Michał", 
-        lastName: "Kowalski", 
-        email: "mkowalski@gmail.com",
-        subject:"RiPO", 
-        tags:"Serafin Lorem Lorem Lorem Lorem Lorem Lorem", 
-        description: "Poszukuje osoby do projektu która ogarnia openCV, pythona venv itp itd "
-    },
-
-    {
-        id: 4, 
-        firstName: "Kacper", 
-        lastName: "Kowalski", 
-        email: "kkowalski@gmail.com",
-        subject:"BD2",  
-        tags:"C#", 
-        description: "Poszukuje osoby do projektu "
-    },
-
-    {
-        id: 5, 
-        firstName: "Michał", 
-        lastName: "Wiśniewski", 
-        email: "wisnia@gmail.com", 
-        subject:"SO2",
-        tags:"Szandała", 
-        description: "Poszukuje osoby do projektu z SO"
-    },
-
-    {
-        id: 6, 
-        firstName: "Jan", 
-        lastName: "Kowalski", 
-        email: "jkowalski@gmail.com",
-        subject:"PIW", 
-        tags:"PIW", 
-        description: "Poszukuje osoby do projektu "
-    },
-    
-    {
-        id: 7, firstName: "Grzegorz", 
-        lastName: "Kowalski", 
-        email: "gkowalski@gmail.com", 
-        subject:"SO2",
-        tags:"Szandała", 
-        description: "Poszukuje osoby do projektu "
-    },
-
-];
-
-const StudentService = {
-
-    getStudents: () => {
-        return students;
-    },
-
-    addStudent: (student) => {
-        students.push(student);
-    }
+// Get all students from json file
+export function getStudents(){
+    return(
+        fetch('data.json',{method: "get"})
+        .then(response => response.json())
+        .then(json => json.students)
+    )
 }
 
-export default StudentService;
+// Add student to json file
+export function addStudent(body){
+    return (
+        fetch('data.json', {
+            method: 'post',
+            body: JSON.stringify(body),
+            headers: {
+              'Content-type': 'application/json'
+            }
+        })
+    )
+}
+
